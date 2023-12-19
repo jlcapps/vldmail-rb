4e5f960cbb1fec7b46a3743044ef302561475aca
Lee Capps <himself@leecapps.com>
2023-12-19 17:35:57 -0600

    Wrap libvldmail struct in Validation class
    Use ruby hooks to allocate and deallocate memory for the struct.
    
    The NULL in the call to Data_Wrap_Struct is in place of a pointer to a
    garbage collection mark function. Such a function is only needed if we
    are storing ruby VALUEs in the struct.

--- ext/vldmail/vldmail_validation.c
+++ ext/vldmail/vldmail_validation.c
@@ -1,5 +1,16 @@
 #include "vldmail_validation.h"
 
+static void deallocate(void *c_validation) {
+    free(c_validation);
+}
+
+static VALUE allocate(VALUE klass) {
+    valid_mail_t *c_validation = malloc(sizeof(valid_mail_t));
+
+    return Data_Wrap_Struct(klass, NULL, deallocate, c_validation);
+}
+
 void Init_vldmail_validation() {
     VALUE vm_cVldMailValidation = rb_define_class_under(vm_mVldMail, "Validation", rb_cObject);
+    rb_define_alloc_func(vm_cVldMailValidation, allocate);
 }
